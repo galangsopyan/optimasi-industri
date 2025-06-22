7444aea58deae920592c184ab4b43078919885e5
galangsopyan <galangsopyan05@gmail.com>
2025-06-22 11:29:04 +0700

    Update optimasi_industri.py

--- optimasi_industri.py
+++ optimasi_industri.py
@@ -21,11 +21,11 @@ with st.form("input_form"):
     col1, col2 = st.columns(2)
 
     with col1:
-        profit_A = st.number_input("Keuntungan per unit Blender (Rp)", key="profit_A", value=40000, step=1000, min_value=0)
+        profit_A = st.number_input("Keuntungan per unit Blender (Rp)", key="profit_A", value=60000, step=1000, min_value=0)
         time_A = st.number_input("Waktu mesin per unit Blender(jam)", key="time_A", value=2.0, step=0.1, min_value=0.1)
     
     with col2:
-        profit_B = st.number_input("Keuntungan per unit Pemanggang Roti (Rp)", key="profit_B", value=60000, step=1000, min_value=0)
+        profit_B = st.number_input("Keuntungan per unit Pemanggang Roti (Rp)", key="profit_B", value=80000, step=1000, min_value=0)
         time_B = st.number_input("Waktu mesin per unit Pemanggang Roti(jam)", key="time_B", value=3.0, step=0.1, min_value=0.1)
     
     total_time = st.number_input("Total jam mesin tersedia per minggu", key="total_time", value=100.0, step=1.0, min_value=1.0)
